9a5c841028b82224ff174aaae97d8786799866f5
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2022-02-02 10:10:36 -0600

    added card css to adopt page and mapped over for photos

--- src/App.js
+++ src/App.js
@@ -16,13 +16,6 @@ import UserContext from './contexts/UserContext';
 // curl -d "grant_type=client_credentials&client_id=U9LdrXEh3kfDyJcOchJ6IhdXvtw7fIaVvlVj6rt4t4BmVo6efX&client_secret=Y5lS7OxvO8L2tZrEuWOTgAw2rw9AZ9HiAbCDT7S2" https://api.petfinder.com/v2/oauth2/token
 // USE THIS TO GET A NEW TOKEN WHEN IT EXPIRES IN 1 HOUR
 
-
-// petfinder API key: U9LdrXEh3kfDyJcOchJ6IhdXvtw7fIaVvlVj6rt4t4BmVo6efX
-
-// petfinder Secret: Y5lS7OxvO8L2tZrEuWOTgAw2rw9AZ9HiAbCDT7S2
-
-// CURRENT access_token : eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJVOUxkclhFaDNrZkR5SmNPY2hKNkloZFh2dHc3ZklhVnZsVmo2cnQ0dDRCbVZvNmVmWCIsImp0aSI6ImI4N2M0YTllNGE5YWNmYWNmODlhMjhlNjYzMTRjYTk2ZDI0MjcxYTEyM2YxNTNkZmU4MDJkOTQxOGQ4MDQ3NjdkZjc4NGUxMDkyMWYyMWZlIiwiaWF0IjoxNjQzNzQ4NjU2LCJuYmYiOjE2NDM3NDg2NTYsImV4cCI6MTY0Mzc1MjI1Niwic3ViIjoiIiwic2NvcGVzIjpbXX0.OQ2h8V74ugyV0abGvsRNxwUrQhVNM0u2LxPt_MrvzhW4BogzltzabR-OfgMQXvR9oqOhv-is9P21XTcM4xhuajffKzB9loMlWLObnv8f59ks2t7Rof7sC8Nc2Y2XN_TNiJNN1CF2TVNgrQ72MMutXGfUr3VOjaK12hP-tYRI01ODII74JN8ABWMANSHldOVrKna0TCKzYPys5htBfqcyQR_rcbZV09y1DxESb4BhS82By03q6JO97ZN3IEZMg4geiM5Q4sY7Ugb7Vc6kwXhcGscuHo6iqsbnMsxtghfVy_a4AheSza-ybMfkuJSHn-_B_xKhP_3iRDOxQhqf5Urs6g
-
 // access token is the token itself; will need to have system store it as a variable and include it in the header of every API request until it expires and we request another
 // TOKEN ONLY LASTS 1 HOUR
 
@@ -34,7 +27,7 @@ const App = () => {
   const [adopts, setAdopts] = useState([])
   const [favorites, setFavorites] = useState([])
   
-  const tokenAccess = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJVOUxkclhFaDNrZkR5SmNPY2hKNkloZFh2dHc3ZklhVnZsVmo2cnQ0dDRCbVZvNmVmWCIsImp0aSI6ImI4N2M0YTllNGE5YWNmYWNmODlhMjhlNjYzMTRjYTk2ZDI0MjcxYTEyM2YxNTNkZmU4MDJkOTQxOGQ4MDQ3NjdkZjc4NGUxMDkyMWYyMWZlIiwiaWF0IjoxNjQzNzQ4NjU2LCJuYmYiOjE2NDM3NDg2NTYsImV4cCI6MTY0Mzc1MjI1Niwic3ViIjoiIiwic2NvcGVzIjpbXX0.OQ2h8V74ugyV0abGvsRNxwUrQhVNM0u2LxPt_MrvzhW4BogzltzabR-OfgMQXvR9oqOhv-is9P21XTcM4xhuajffKzB9loMlWLObnv8f59ks2t7Rof7sC8Nc2Y2XN_TNiJNN1CF2TVNgrQ72MMutXGfUr3VOjaK12hP-tYRI01ODII74JN8ABWMANSHldOVrKna0TCKzYPys5htBfqcyQR_rcbZV09y1DxESb4BhS82By03q6JO97ZN3IEZMg4geiM5Q4sY7Ugb7Vc6kwXhcGscuHo6iqsbnMsxtghfVy_a4AheSza-ybMfkuJSHn-_B_xKhP_3iRDOxQhqf5Urs6g'
+  const tokenAccess = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJVOUxkclhFaDNrZkR5SmNPY2hKNkloZFh2dHc3ZklhVnZsVmo2cnQ0dDRCbVZvNmVmWCIsImp0aSI6ImJlNGQ1YmNhYjhjMzNjMGZiMTBiZGZmNzQwNzczODk5MzcwOWVkYTE4Nzk0MWNhYjlkMmJjMzAzNWI0NTg2MzVkZWE3ZDE5MzE0NGViZWRiIiwiaWF0IjoxNjQzODE2ODg0LCJuYmYiOjE2NDM4MTY4ODQsImV4cCI6MTY0MzgyMDQ4NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.dYrmv-vejbKQpkddfky9cHX_B_lXLGjGaEbV_XMvaFPCUjukzHFbNsWPiHMTJZxWKYd9abUbSDLHp_BM5CC3kawuJwEhSrJjNcRO64kICgdmM-KozZwXYQN-Gp9uuljhzLxRiTnPIN-U3AtML_iVCsAOnr0oJ8tReoLhx1ZfVuiJKlaP6FCryIQ5G6QE56exGZTzYiTCNQLsgIfXRfNhqs7Gij0K9GA1P19Hff1879V1Vd8eq_x93Wh_1EpbwZ5ebUjKxawxGs6gF6AgL7JjkOLWofvWpx97i2qwZzhtIvDuVdoditaHH6garEx-3xNTtV5FGmPxffyDdW2GATedrg'
 
   const fetchAdopts = async () => {
     try {
@@ -45,7 +38,6 @@ const App = () => {
       }
       )
       setAdopts(res.data.animals)
-      // console.log(res.data.animals)
     } catch (error) {
       console.log(error)
     }
--- src/pages/Adoptables/index.js
+++ src/pages/Adoptables/index.js
@@ -7,17 +7,35 @@ const Adoptables = ({ adopts }) => {
     console.log(adopts)
 
     return (
-        <div>
-            Pets to Adopt go Here!
-            {
-                adopts.map(adopts => (
-                    <div key={adopts?.id}>
-                        {adopts.name}
-                    </div>
-                ))
-            }
-        </div>
+        <div className='adopts-container'>
+                {
+                    adopts.map(adopts => (
+                        <div className="card adopt-card" key={adopts.id}>
+
+                            <img src={adopts.primary_photo_cropped ?
+                                adopts.primary_photo_cropped?.small  :
+                                null} className="card-img-top" alt="..." />
+
+                            <div className="card-body">
+                                <h5 className="card-title">{adopts.name}</h5>
+                                <p className="card-text">ID: {adopts.id}</p>
+                                <p className="card-text">Gender: {adopts.gender}</p>
+                                <button className="btn btn-danger">Like</button>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
     );
 }
 
 export default Adoptables;
+
+// {
+//     adopts.map(adopts => (
+//         <div key={adopts?.id}>
+//             {adopts.name}
+//         </div>
+//     ))
+// }
